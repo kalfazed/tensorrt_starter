76a4176f6d650ffa8bc71be160a279cfa9cc6134
kalfazed <kalfazed@gmail.com>
2023-06-11 12:48:49 +0900

    added traverse of 2D cord in cuda

--- 2.1-dim_and_index/src/print_index.cu
+++ 2.1-dim_and_index/src/print_index.cu
@@ -41,8 +41,22 @@ __global__ void print_thread_idx_kernel(){
          bIndex, tIndex, index);
 }
 
+__global__ void print_cord_kernel(){
+  int index = threadIdx.z * blockDim.x * blockDim.y + \
+              threadIdx.y * blockDim.x + \
+              threadIdx.x;
+
+  int x  = blockIdx.x * blockDim.x + threadIdx.x;
+  int y  = blockIdx.y * blockDim.y + threadIdx.y;
+
+  printf("block idx: (%3d, %3d, %3d), thread idx: %3d, cord: (%3d, %3d)\n",
+         blockIdx.z, blockIdx.y, blockIdx.x,
+         index, x, y);
+
+}
+
 void print_one_dim(){
-  int inputSize = 32;
+  int inputSize = 8;
 
   int blockDim = 4;
   int gridDim = inputSize / blockDim;
@@ -51,9 +65,9 @@ void print_one_dim(){
   dim3 grid(gridDim);
 
   print_idx_kernel<<<grid, block>>>();
-  print_dim_kernel<<<grid, block>>>();
-  print_thread_idx_per_block_kernel<<<grid, block>>>();
-  print_thread_idx_kernel<<<grid, block>>>();
+  // print_dim_kernel<<<grid, block>>>();
+  // print_thread_idx_per_block_kernel<<<grid, block>>>();
+  // print_thread_idx_kernel<<<grid, block>>>();
 
   cudaDeviceSynchronize();
 }
@@ -68,9 +82,23 @@ void print_two_dim(){
   dim3 grid(gridDim, gridDim);
 
   print_idx_kernel<<<grid, block>>>();
-  // print_dim_kernel<<<grid, block>>>();
-  // print_thread_idx_per_block_kernel<<<grid, block>>>();
-  // print_thread_idx_kernel<<<grid, block>>>();
+  print_dim_kernel<<<grid, block>>>();
+  print_thread_idx_per_block_kernel<<<grid, block>>>();
+  print_thread_idx_kernel<<<grid, block>>>();
+
+  cudaDeviceSynchronize();
+}
+
+void print_cord(){
+  int inputWidth = 4;
+
+  int blockDim = 2;
+  int gridDim = inputWidth / blockDim;
+
+  dim3 block(blockDim, blockDim);
+  dim3 grid(gridDim, gridDim);
+
+  print_cord_kernel<<<grid, block>>>();
 
   cudaDeviceSynchronize();
 }
@@ -85,6 +113,7 @@ int main() {
     __syncthreads:         线程块内同步
   */
   // print_one_dim();
-  print_two_dim();
+  // print_two_dim();
+  print_cord();
   return 0;
 }
