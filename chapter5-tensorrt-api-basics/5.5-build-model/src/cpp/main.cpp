e99b7bf4c3d50ac59251070ff143ea2532c79b57
Phoenix <fengzhe8215@foxmail.com>
2024-06-28 11:37:44 +0800

    Add some new examples in chapter5/5.5 (#8)
    * Squashed commit of the following:
    
    commit 27de6487a4b25369445410c58b802260ed2efff3
    Author: FengZhe <fengzhe8215@foxmail.com>
    Date:   Thu Mar 7 20:22:10 2024 +0800
    
        remove exit(1) in logger function
    
    commit 63ae387bbde8c8d4bd3ea9bd586fc10f4fda9911
    Author: FengZhe <fengzhe8215@foxmail.com>
    Date:   Tue Mar 5 23:17:45 2024 +0800
    
        remove some unused variables
    
    * Added dynamic parameters to the `build_concat` and `build_slice` functions.

--- chapter5-tensorrt-api-basics/5.5-build-model/src/cpp/main.cpp
+++ chapter5-tensorrt-api-basics/5.5-build-model/src/cpp/main.cpp
@@ -23,7 +23,14 @@ int main(int argc, char const *argv[])
     // Model model("models/weights/sample_permute.weights");
     // Model model("models/weights/sample_reshape.weights");
     // Model model("models/weights/sample_batchNorm.weights");
-    Model model("models/weights/sample_cbr.weights");
+    // Model model("models/weights/sample_cbr.weights");
+    // Model model("models/weights/sample_pooling.weights");
+    // Model model("models/weights/sample_upsample.weights");
+    // Model model("models/weights/sample_deconv.weights");
+    // Model model("models/weights/sample_concat.weights");
+    // Model model("models/weights/sample_elementwise.weights");
+    // Model model("models/weights/sample_reduce.weights");
+    Model model("models/weights/sample_slice.weights");
 
     if(!model.build()){
         LOGE("fail in building model");
